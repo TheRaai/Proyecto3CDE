eb606a8d5e915c4608408ed7857855c4ed1ecf77
tcantuarias <54869477+tcantuarias@users.noreply.github.com>
2022-06-30 17:19:41 -0400

    subi una version antigua :$

--- limpieza de datos.R
+++ limpieza de datos.R
@@ -1,4 +1,4 @@
-pacman::p_load(janitor, data.validator, sf, stats, tidyverse, dplyr)
+pacman::p_load(sf, stats, tidyverse, dplyr)
 
 
 mapa <- read_sf("C:/Users/tomas/OneDrive - Universidad Adolfo Ibanez/U/11/ciencia de datos espaciales/proyecto 3/RM_SII_CBR2/RM_SII_CBR2.shp")
@@ -11,3 +11,12 @@ as.data.frame(colnames(mapa_pennalolen))
 
 mapa_pennalolen <- subset(mapa_pennalolen, select = -c(40, 41)) # Se eliminan columnas casi vacías
 
+mapa_pennalolen <- subset(mapa_pennalolen, select = -c(23,24,25,26,27,28,29,30)) #se eliminan columnas referentes al conservador de bienes raices
+mapa_pennalolen <-subset(mapa_pennalolen, select = -c(1,3,27,28)) #se eliminan las columnas comuna
+
+#Se asume que las en la columna depto casa los valores == 0 son las casas
+mapa_pennalolen <- filter(mapa_pennalolen, DPTO_CASA==0)
+mean(mapa_pennalolen$Score)
+mapa_pennalolen <-subset(mapa_pennalolen, select = -c(1)) #se elimina score por tener el mismo valor en todas las variables
+
+write.csv(mapa_pennalolen, "C:/Users/tomas/OneDrive - Universidad Adolfo Ibanez/U/11/ciencia de datos espaciales/proyecto 3/mapa_pennalolen.csv")
